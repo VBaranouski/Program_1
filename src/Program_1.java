df6bb183812f6d82ab7389a044ca2ecca9207152
Vlad <vladislav.baranovski@gmail.com>
2017-07-17 17:00:53 +0300

    Initial commit

---
+++ src/Program_1.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vlad on 7/17/17.
+ */
+public class Program_1 {
+    public static void main(String args[]) {
+        System.out.print("Hello ");
+        double a = 6.0, b = 6.0;
+        double c = Math.sqrt(a*b);
+        System.out.print(c);
+    }
+}
