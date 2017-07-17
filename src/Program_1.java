42e1282e7ce7fae4a18e20d0ba2e088619689b90
Vlad <vladislav.baranovski@gmail.com>
2017-07-17 17:02:30 +0300

    update in values

--- src/Program_1.java
+++ src/Program_1.java
@@ -4,7 +4,7 @@
 public class Program_1 {
     public static void main(String args[]) {
         System.out.print("Hello ");
-        double a = 6.0, b = 6.0;
+        double a = 6.0, b = 5.0;
         double c = Math.sqrt(a*b);
         System.out.print(c);
     }
